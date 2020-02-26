cde795fb2c83b211b0e25638e376d333271deb01
mengxuan <kit.mma@gmail.com>
2020-02-25 22:49:45 -0800

    login and logout

--- src/rpc/Register.java
+++ src/rpc/Register.java
@@ -33,7 +33,7 @@ public class Register extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		MySQLConnection connection = new MySQLConnection();
+		// MySQLConnection connection = new MySQLConnection();
 		try {
 			JSONObject input = RpcHelper.readJSONObject(request);
 			String userid = input.getString("user_id");
@@ -42,16 +42,17 @@ public class Register extends HttpServlet {
 			String lastname = input.getString("lastname");
 			
 			JSONObject obj = new JSONObject();
-			if (connection.registerUser(userid, password, firstname, lastname)) {
-				obj.put("status", "ok");
-			}else {
-				obj.put("status", "User Already Exists");
-			}
+//			if (connection.registerUser(userid, password, firstname, lastname)) {
+//				obj.put("status", "ok");
+//			}else {
+//				obj.put("status", "User Already Exists");
+//			}
 			RpcHelper.writeJsonObject(response, obj);
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
-			connection.close();
+			// connection.close();
+			System.out.print("connection closed");
 		}
 	}
 
