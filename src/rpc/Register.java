c67095dac9b21ebd40d90d732d11dcbf1f0ea8e7
lifu-yao <yaoleaf@gmail.com>
2020-02-28 23:52:59 -0800

    updated

--- src/rpc/Register.java
+++ src/rpc/Register.java
@@ -18,40 +18,43 @@ import db.MySQLConnection;
 @WebServlet("/register")
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Register() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Register() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		MySQLConnection connection = new MySQLConnection();
 		try {
 			JSONObject input = RpcHelper.readJSONObject(request);
-			String userid = input.getString("user_id");
+			String userId = input.getString("user_id");
 			String password = input.getString("password");
-			String firstname = input.getString("firstname");
-			String lastname = input.getString("lastname");
-			
+			String firstName = input.getString("first_name");
+			String lastName = input.getString("last_name");
+
 			JSONObject obj = new JSONObject();
-			if (connection.registerUser(userid, password, firstname, lastname)) {
+			if (connection.registerUser(userId, password, firstName, lastName)) {
 				obj.put("status", "ok");
-			}else {
+			} else {
 				obj.put("status", "User Already Exists");
 			}
 			RpcHelper.writeJsonObject(response, obj);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			connection.close();
